e8de774c53970d8b0b962940d71f12dc17bfddf0
kush <muchiri.patrick@students.jkuat.ac.ke>
2023-09-14 05:22:01 -0700

    prining using put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main-it prints output using put command
+ *
+ *return:always (0)
+ *
+ * return 0 on succesful excecution
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
